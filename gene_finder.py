80cf45f9591ce5a709f4a8c5ca4e66f282ea04c5
John Moreland <jmoreland@outlook.com>
2016-01-31 23:51:47 -0500

    completed first 6 functions

--- gene_finder.py
+++ gene_finder.py
@@ -2,7 +2,7 @@
 """
 YOUR HEADER COMMENT HERE
 
-@author: YOUR NAME HERE
+@author: john moreland
 
 """
 
@@ -29,10 +29,23 @@ def get_complement(nucleotide):
     'T'
     >>> get_complement('C')
     'G'
+    >>> get_complement('T')
+    'A'
+    >>> get_complement('G')
+    'C'
     """
     # TODO: implement this
     pass
 
+    if nucleotide == "A" or nucleotide == "C" or nucleotide == "T" or nucleotide == "G":
+    	if nucleotide == "A":
+    		return "T" 
+    	elif nucleotide == "C": 
+    		return "G"
+    	elif nucleotide == "T":
+    		return "A"
+    	elif nucleotide == "G":
+    		return "C"
 
 def get_reverse_complement(dna):
     """ Computes the reverse complementary sequence of DNA for the specfied DNA
@@ -48,6 +61,24 @@ def get_reverse_complement(dna):
     # TODO: implement this
     pass
 
+    stringlength = len(dna)
+    # print stringlength
+    t = 0
+    u = 1
+    complementedstring = ''
+    finalstring = ''
+    #get complements of each nucleotide in the string
+    while t < stringlength:
+    	complementedstring = complementedstring + get_complement(dna[t])
+    	t = t+1
+    #reverse the string
+    while u < stringlength+1:
+    	finalstring = finalstring + (complementedstring[stringlength-u])
+    	u = u+1
+    # print finalstring
+    return finalstring
+
+
 
 def rest_of_ORF(dna):
     """ Takes a DNA sequence that is assumed to begin with a start
@@ -61,9 +92,70 @@ def rest_of_ORF(dna):
     'ATG'
     >>> rest_of_ORF("ATGAGATAGG")
     'ATGAGA'
+    >>> rest_of_ORF("ATGAAGATAGG") #stop codon present, but not in this frame
+    'ATGAAGATAGG'
     """
     # TODO: implement this
     pass
+
+    i=0
+    
+    # print dna
+
+    TAA = 1							#initialize variables
+    storeTAA = 0
+    while 1 == 1:					#start a loop
+    	TAA = dna.find('TAA', TAA+1)	#search for "TAA" in dna string, starting at TAA value
+    	if TAA == storeTAA:			#if loop gets the same value after two run throughs, it is an invalid value, so -1
+    		TAA = -1
+    	storeTAA = TAA 				#store the TAA value so it can be referenced again
+        if TAA < 0: 				#if value is invalid (-1), just break
+            break
+        elif (TAA % 3) == 0: 		#if the value is valid (divisible by 3) keep it and break
+            break
+    # print('TAA', TAA)
+
+    TAG = 1
+    storeTAG = 0
+    while 1 == 1:
+    	TAG = dna.find('TAG', TAG+1)
+    	# print(TAG)
+    	if TAG == storeTAG:
+    		TAG = -1
+    	storeTAG = TAG
+        if TAG < 0:
+            break
+        elif (TAG % 3) == 0:
+            break
+    # print('TAG', TAG)
+
+    TGA = 0
+    storeTGA = 0
+    while 1 == 1:
+    	TGA = dna.find('TGA', TGA+1)
+    	# print(TGA)
+    	if TGA == storeTGA:
+    		TGA = -1
+    	storeTGA = TGA
+        if TGA < 0:
+            break
+        elif (TGA % 3) == 0:
+            break
+    # print('TGA', TGA)
+
+    readingframe = ""
+    if TAA > 0 or TAG > 0 or TGA > 0: #if a stop codon is present
+    	#return values up to stop codon
+    	while i < len(dna):
+    		readingframe = readingframe + dna[i]
+    		if i>0 and (i == TAA-1 or i == TAG-1 or i == TGA-1):
+    			# print readingframe
+    			return readingframe
+    		else:
+    			i = i + 1
+     #else return whole string
+    else:
+    	return dna
 
 
 def find_all_ORFs_oneframe(dna):
@@ -81,6 +173,28 @@ def find_all_ORFs_oneframe(dna):
     """
     # TODO: implement this
     pass
+    i = 0
+    output=[]
+    start_index = 0
+
+    #search for an ATG  
+    while 1==1:
+	    start_index = dna.find('ATG', start_index)
+	    # print start_index
+
+	    if start_index % 3 == 0: #if start index starts on an actual codon
+	    	#run rest_of_ORF to get the whole frame
+	    	store = rest_of_ORF(dna[start_index:])
+	    	#save that ORF
+	    	output.append(store)
+	    	#new start index = index of current ORF + lenth of ORF + 3 (stop codon)
+	    	start_index = start_index + len(store) + 3
+	    	i=i+1
+	    elif start_index == -1:
+	    	# print output
+	    	return output
+	    else:
+	    	start_index = start_index+1
 
 
 def find_all_ORFs(dna):
@@ -99,6 +213,9 @@ def find_all_ORFs(dna):
     # TODO: implement this
     pass
 
+    return find_all_ORFs_oneframe(dna) + find_all_ORFs_oneframe(dna[1:]) +find_all_ORFs_oneframe(dna[2:])
+
+
 
 def find_all_ORFs_both_strands(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence on both
@@ -111,6 +228,8 @@ def find_all_ORFs_both_strands(dna):
     """
     # TODO: implement this
     pass
+
+    return find_all_ORFs(get_reverse_complement(dna)) + find_all_ORFs(dna)
 
 
 def longest_ORF(dna):
@@ -161,6 +280,16 @@ def gene_finder(dna):
     # TODO: implement this
     pass
 
+
+
+
+
+# get_reverse_complement("ATGCGAATGTAGCATCAAA")
+# find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
+get_reverse_complement("CCGCGTTCA")
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    # doctest.testmod()
+    # doctest.run_docstring_examples(rest_of_ORF, globals())
+
+
